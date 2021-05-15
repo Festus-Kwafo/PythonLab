7974c171c4beb41674e2de91dc705ad5a1f91a0d
Festus Kwafo <68119922+Festus-Kwafo@users.noreply.github.com>
2021-05-15 00:56:52 +0000

    Add files via upload

---
+++ fibonacci_sequence.py
@@ -0,0 +1,32 @@
+import math
+class Fibonacci:
+    def __init__(self, userTerm):
+        self.userTerm = userTerm
+
+    def fTerm(self):
+        f1 = (1 + math.sqrt(5))/2
+        ans_f1 = f1**userTerm
+        return ans_f1
+        
+        
+    def sTerm(self):
+        f2= (1 - math.sqrt(5))/2
+        ans_f2 = f2**userTerm
+        return ans_f2
+     
+
+userTerm = int (input("Enter term: "))
+limit = userTerm
+while limit > 0:
+    
+    Mycal = Fibonacci(userTerm)
+    numerator = Mycal.fTerm()-Mycal.sTerm()
+    denominator = math.sqrt(5)
+    fn = int (numerator/denominator)
+    print("The value of the "+ str(userTerm)+"th term is: " + str (fn))
+    userTerm = userTerm-1
+    limit = limit-1 
+
+
+
+
