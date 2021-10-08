26a563f120642ad4e0cc80cabe3b2bb0decfd3e9
Festus Kwafo <68119922+Festus-Kwafo@users.noreply.github.com>
2021-10-08 11:24:45 +0000

    Updated palindrome

--- palindrome.py
+++ palindrome.py
@@ -1,11 +1,11 @@
-#A string is a palindrome if it is identical forward and backward. For example
-#“anna”,“civic”,“level”and“hannah”are all examples of palindromic words.Write a
-#program that reads a string from the user and uses a loop to determines whether or
-#not it is a palindrome.
+# A string is a palindrome if it is identical forward and backward. For example
+# “anna”,“civic”,“level”and“hannah”are all examples of palindromic words.Write a
+# program that reads a string from the user and uses a loop to determines whether or
+# not it is a palindrome.
 
-text = str(input("Enter any word: "))
-while text == text[::-1]:
-    print("The word "+ text + " is palindrome")
+txt = str(input("Enter any word: "))
+while txt == txt[::-1]:
+    print("The word " + txt + " is palindrome")
     break
-else: 
-    print("The word "+ text + " is not palindrome")
+else:
+    print("The word " + txt + " is not palindrome")
